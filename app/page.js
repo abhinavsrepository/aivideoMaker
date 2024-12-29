eeecf4684ecc8b18ffcd5afc0d6f07202eda188d
Abhinav Singh <abhi2510979b@gmail.com>
2024-12-29 17:51:12 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">hell
-      </div>
+    <Button variant="destructive">hel</Button>
   );
 }
---
+++ configs/schema.js
@@ -0,0 +1,10 @@
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const Users=pgTable('users',{
+    id:serial("id").primaryKey(),
+    name:varchar('name').notNull(),
+    email:varchar('email').notNull(),
+    imageUrl:varchar('image_url'),
+    subscription:boolean('subscription').default(false)
+    
+})
